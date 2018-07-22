6a3adfdbf9701dddb3e922836051215f02b660c9
Andre Miranda <andre42m@gmail.com>
2018-07-22 15:30:53 -0300

    Update autotools setup based on sample-plugin

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 #
-# $Id$
+# Copyright (c) 2002-2018 The Xfce development team. All rights reserved.
 #
-# Copyright (c) 2002-2007
-#         The Xfce development team. All rights reserved.
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Written for Xfce by Benedikt Meurer <benny@xfce.org>.
 #
 
 (type xdt-autogen) >/dev/null 2>&1 || {
@@ -11,23 +24,12 @@
 autogen.sh: You don't seem to have the Xfce development tools installed on
             your system, which are required to build this software.
             Please install the xfce4-dev-tools package first, it is available
-            from http://www.xfce.org/.
+            from https://www.xfce.org/.
 EOF
   exit 1
 }
 
-# verify that po/LINGUAS is present
-(test -f po/LINGUAS) >/dev/null 2>&1 || {
-  cat >&2 <<EOF
-autogen.sh: The file po/LINGUAS could not be found. Please check your snapshot
-            or try to checkout again.
-EOF
-  exit 1
-}
+test -d m4 || mkdir m4
 
-# substitute revision and linguas
-linguas=`sed -e '/^#/d' po/LINGUAS`
-sed -e "s/@LINGUAS@/${linguas}/g" \
-    < "configure.in.in" > "configure.in"
-
+XDT_AUTOGEN_REQUIRED_VERSION="4.9.1" \
 exec xdt-autogen $@
